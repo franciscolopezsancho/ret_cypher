f24f98bfd9f70dc61306ecb168cea217a7bb6e35
franciscolopezsancho <franciscolopezsancho@gmail.com>
2015-06-21 18:28:48 +0100

    second commit. refactoring

--- src/main/scala/com/cognia/cypher/Guess.scala
+++ src/main/scala/com/cognia/cypher/Guess.scala
@@ -11,69 +11,11 @@ import scala.collection.immutable.Stream._
 class Guess {
 
 
-   //TODO pass functions!! with a signature (key)(word)
-  def bestGuess(line:List[String],decypher:Int=>String=>String): Int ={
-    def bestGuess(line:List[String],bestFoundRet:(Int,Int),ret:Int,decypher:Int=>String=>String):Int={
-      if(    (bestFoundRet._1.toFloat./(line.length.toFloat)).toDouble.round == 1) return bestFoundRet._2
-      if(ret == 0) bestFoundRet._2
-      else {
-        //counting total words we find in the dictionary
-        val totalFound = line.foldLeft(0)( (acc,word) =>
-          acc + Reader.inDictionary(decypher(ret)(word))
-        )
-        val best = if (bestFoundRet._1 > totalFound) bestFoundRet else (totalFound,ret)
-        bestGuess(line,best,ret-1,decypher)
-      }
-    }
-     //TODO avoid coupling!!!!!
-    val best = bestGuess(line,(0,0),25,decypher)
-    // println("best "+best)
-    // println("line "+line)
-    if(best <= 1){
-      println("###########  "+best)
-    }
-    best
-  }
-
-  //TODO  check first word if not best guess
-  def sameOld(word:String,guess:Int):Boolean={
-    Reader.inDictionaryWord(Decrypter.decrypt(guess)(retainJustLetters(word)))
-  }
-
-  def firstGuess(line:List[String],guess:Int): Boolean ={
-    //line.flatten match {case Nil => 1L}
-    val ratio = line.foldLeft(0)( (acc,word) =>
-      acc + Reader.inDictionary(Decrypter.decrypt(guess)(retainJustLetters(word)))
-    )
-    (ratio.toFloat./(line.length.toFloat)).toDouble.round == 1
-  }
-
-  def retainJustLetters(str:String): String ={
-    str.toCharArray.filter(p => p.toUpper >= 'A' && p.toUpper <= 'Z').mkString
-  }
 
 
 
-//  def doSome(): Unit ={
-//    println("first lines ------------ encoding")
-//    Reader.in.getLines().toStream.take(19).foreach( f =>
-//      println(f.getBytes.mkString)
-//    )
-//    println("first lines ------------ encoding")
-//
-//            Reader.in.getLines().toStream.drop(19).foreach( line =>
-//              //val pastBestGuess =  bestGuess(line.split(" ").toList)
-//                println(Decrypter.decrypt(bestGuess(line.split(" ").toList,Decrypter.decrypt))(line))
-//
-//            )
-//
-//
-//
-//  }
-//
-//  def doSomeTrad(): Unit ={
-//    trad(Reader.in.getLines().toStream.drop(19),Decrypter.decrypt).foreach(println)
-//  }
+
+
 
   //in order to check the last line avoiding state
   //and therefore concurrency race conditions
@@ -92,7 +34,7 @@ class Guess {
       }
     }
     def tryLast(words:List[String],lastGuess:Int): Int = {
-      if(words.isEmpty || (words.size == 1 && words.head.isEmpty) || sameOld(words.head,lastGuess) || firstGuess(words.toList,lastGuess)){
+      if(words.isEmpty || (words.size == 1 && words.head.isEmpty) || sameOld(words.head,lastGuess) ){
         lastGuess
       }  else {
         val best = bestGuess(words,decypher)
@@ -107,5 +49,45 @@ class Guess {
     res
   }
 
+  //Look up a dictionary in order to detect
+  //after iterating over diff keys what's
+  //the best guess. The key that after decrypt
+  // finds more words in a pretty basic dictionary
+  def bestGuess(line:List[String],decypher:Int=>String=>String): Int = {
+    def bestGuess(line: List[String], bestFoundRet: (Int, Int), ret: Int, decypher: Int => String => String): Int = {
+      if ((bestFoundRet._1.toFloat./(line.length.toFloat)).toDouble.round == 1) return bestFoundRet._2
+      if (ret == 0) bestFoundRet._2
+      else {
+        //counting total words we find in the dictionary
+        val totalFound = line.foldLeft(0)((acc, word) =>
+          acc + Reader.inDictionary(decypher(ret)(word))
+        )
+        val best = if (bestFoundRet._1 > totalFound) bestFoundRet else (totalFound, ret)
+        bestGuess(line, best, ret - 1, decypher)
+      }
+    }
+    bestGuess(line, (0, 0), 25, decypher)
+  }
+
+
+  //TODO  check a word is after decrypted in a dictiory
+  //mainly used to avoid to do a best guess if first word
+  //of a line can be found in the dictionary using the last
+  //guess
+  def sameOld(word:String,lastGuess:Int):Boolean={
+    Reader.inDictionaryWord(Decrypter.decrypt(lastGuess)(retainJustLetters(word)))
+  }
+
+
+//  def firstGuess(line:List[String],guess:Int): Boolean ={
+//    val ratio = line.foldLeft(0)( (acc,word) =>
+//      acc + Reader.inDictionary(Decrypter.decrypt(guess)(retainJustLetters(word)))
+//    )
+//    (ratio.toFloat./(line.length.toFloat)).toDouble.round == 1
+//  }
+
+  def retainJustLetters(str:String): String ={
+    str.toCharArray.filter(p => p.toUpper >= 'A' && p.toUpper <= 'Z').mkString
+  }
 
 }
